c076258ef1cc42ad12f6ae93cc805e7487471b9f
Brandon Hazelton <hazeltonbw@gmail.com>
2023-01-05 01:04:17 -0800

    delete old commented out code

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-// const express = require("express");
-// //const morgan = require("morgan");
-// const bodyParser = require("body-parser");
-
-// const app = express();
-// const port = process.env.PORT || 4000;
-
-// app.use(morgan("combined"));
-// app.use(bodyParser.json());
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: true,
-//   })
-// );
-
-// app.get("/", (req, res) => {
-//   //res.send("Hello World!");
-//   res.json({});
-// });
-
-// app.get("/users", db.getUsers);
-// app.get("/users/:id", db.getUserById);
-// app.post("/users", db.createUser);
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
 const express = require("express");
 const app = express();
 
