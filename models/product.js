c7ce114930a166806099f97decc0201c194b0d8b
Brandon Hazelton <hazeltonbw@gmail.com>
2023-01-08 00:24:57 -0800

    implemented product model

---
+++ models/product.js
@@ -0,0 +1,36 @@
+const pool = require("../db");
+
+const getProducts = async () => {
+  try {
+    const statement = `
+      SELECT * FROM products 
+        ORDER BY id ASC`;
+    const result = await pool.query(statement);
+    return result.rows;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+const getProductById = async (id) => {
+  const statement = `
+      SELECT * FROM products 
+      WHERE id = $1 `;
+  const values = [id];
+
+  try {
+    const result = await pool.query(statement, values);
+    if (result.rows?.length) {
+      return result.rows[0];
+    } else {
+      return null;
+    }
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+module.exports = {
+  getProducts,
+  getProductById,
+};
