b60e7d88a5be4bc8a1962029625cea85e3584cdd
Brandon Hazelton <hazeltonbw@gmail.com>
2023-01-08 02:43:00 -0800

    accounted for name, category, and all products

--- routes/products.js
+++ routes/products.js
@@ -7,14 +7,19 @@ module.exports = (app) => {
   app.use("/products", router);
 
   router.get("/", async (req, res, next) => {
-    const category = req.query.category;
+    const { category, name } = req.query;
 
     try {
       let response;
-      category 
-        ? response = await productModel.getProductsByCategory(category) 
-        : response = await productModel.getProducts(category);
-      ;
+      if (category) {
+        response = await productModel.getProductsByCategory(category)
+      }
+      else if (name) {
+        response = await productModel.getProductsByName(name)
+      }
+      else {
+        response = await productModel.getProducts();
+      }
       res.status(200).send(response);
     } catch (err) {
       next(err);
