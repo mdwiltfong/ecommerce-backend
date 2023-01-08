8d7cfcdf9831c7d0bb4a3e63da1c1f10296a71a8
Brandon Hazelton <hazeltonbw@gmail.com>
2023-01-08 00:25:34 -0800

    add productsRouter to routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 // Holds all of the various express routers (users, products, orders)
 const userRouter = require("./users");
 const authRouter = require("./auth");
+const productsRouter = require("./products");
 module.exports = (app, passport) => {
   userRouter(app);
   authRouter(app, passport);
+  productsRouter(app);
 };
