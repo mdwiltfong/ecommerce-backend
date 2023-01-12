3af7e5693bae7c62b6f042513e05829befdffa81
Brandon Hazelton <hazeltonbw@gmail.com>
2023-01-12 00:26:16 -0800

    add a GET / route

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const app = require("./index");
 const { PORT } = require("./config");
 
+app.get("/", (req, res, next) => {
+  res.send(":)))))");
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
 });
