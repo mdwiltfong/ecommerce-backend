44132a5c460e61d99d300ac2760cd5a8454882e4
Brandon Hazelton <hazeltonbw@gmail.com>
2023-01-13 01:57:47 -0800

    add todos

--- routes/users.js
+++ routes/users.js
@@ -6,14 +6,18 @@ const userModel = require("../models/user");
 module.exports = (app) => {
   app.use("/users", router);
 
-  router.get("/", async (req, res, next) => {
-    try {
-      const response = await userModel.getUsers();
-      res.status(200).send(response);
-    } catch (err) {
-      next(err);
+  // TODO: isLoggedIn, isAdmin
+  router.get(
+    "/",
+    /* isLoggedIn, isAdmin,*/ async (req, res, next) => {
+      try {
+        const response = await userModel.getUsers();
+        res.status(200).send(response);
+      } catch (err) {
+        next(err);
+      }
     }
-  });
+  );
 
   router.get("/:id", async (req, res, next) => {
     try {
@@ -26,28 +30,36 @@ module.exports = (app) => {
   });
 
   // Update user password in database
-  router.put("/:id", async (req, res, next) => {
-    const id = parseInt(req.params.id);
-    const { password } = req.body;
-    const user = { id, password };
+  // TODO: isLoggedIn
+  router.put(
+    "/:id",
+    /* isLoggedIn, */ async (req, res, next) => {
+      const id = parseInt(req.params.id);
+      const { password } = req.body;
+      const user = { id, password };
 
-    try {
-      const response = await userModel.updateUserPassword(user);
-      res.status(200).send(response);
-    } catch (err) {
-      next(err);
+      try {
+        const response = await userModel.updateUserPassword(user);
+        res.status(200).send(response);
+      } catch (err) {
+        next(err);
+      }
     }
-  });
+  );
 
-  router.delete("/:id", async (req, res, next) => {
-    const userId = parseInt(req.params.id);
-    try {
-      const response = await userModel.deleteUserById(userId);
-      res.status(200).send({
-        message: `User with userId: ${userId} deleted from database.`,
-      });
-    } catch (err) {
-      next(err);
+  // TODO: isLoggedIn, isAdmin
+  router.delete(
+    "/:id",
+    /*isLoggedIn, isAdmin,*/ async (req, res, next) => {
+      const userId = parseInt(req.params.id);
+      try {
+        const response = await userModel.deleteUserById(userId);
+        res.status(200).send({
+          message: `User with userId: ${userId} deleted from database.`,
+        });
+      } catch (err) {
+        next(err);
+      }
     }
-  });
+  );
 };
