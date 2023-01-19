09256b0f507b6671562a5ce8075b7ef5d9d282e4
Brandon Hazelton <hazeltonbw@gmail.com>
2023-01-19 06:21:31 -0800

    improve testing

--- tests/products.test.js
+++ tests/products.test.js
@@ -2,6 +2,7 @@ const request = require("supertest");
 const app = require("../index");
 
 describe("Products route", () => {
+  // variable setup needed to test product route
   let response;
   let productId;
   const productObject = expect.objectContaining({
@@ -13,6 +14,21 @@ describe("Products route", () => {
   });
   let arrayOfProducts = expect.arrayContaining([productObject]);
   const emptyObject = expect.objectContaining({});
+  const emptyArray = expect.arrayContaining([]);
+  const urls = {
+    good: [
+      "/products?category=fishing",
+      "/products?category=electronics",
+      "/products?name=test",
+      "/products?name=fishing",
+    ],
+    bad: [
+      "/products?category=wgwergrwgrgreg",
+      "/products?name=rgkjergiergreger",
+      "/products?name=3230222g2",
+      "/products?name=_________123",
+    ],
+  };
 
   describe("GET /products", () => {
     beforeAll(async () => {
@@ -30,85 +46,135 @@ describe("Products route", () => {
     });
   });
 
-  describe("GET /products?category=fishing", () => {
-    const arrayOfFishingProducts = expect.arrayContaining([
-      expect.objectContaining({
-        id: expect.any(Number),
-        name: expect.any(String),
-        price: expect.any(String),
-        description: expect.any(String),
-        category: "Fishing",
-      }),
-    ]);
-    beforeAll(async () => {
-      response = await request(app).get("/products?category=fishing");
-    });
-    it("should return HTTP 200", () => {
-      expect(response.statusCode).toBe(200);
+  describe("GET /products?query=something", () => {
+    // two queries could be possible, name and category
+    // see urls object above to see possible queries
+    describe("given a query that matches something in the database", () => {
+      it("should ", async () => {
+        for (const url of urls.good) {
+          response = await request(app).get(url);
+          expect(response.statusCode).toBe(200);
+          expect(response.body).toEqual(arrayOfProducts);
+        }
+      });
     });
 
-    it("should return an array of products within the fishing category", () => {
-      expect(response.body).toEqual(arrayOfFishingProducts);
-    });
-  });
-
-  describe("GET /products?name=test", () => {
-    beforeAll(async () => {
-      response = await request(app).get("/products?name=test");
-    });
-
-    it("should return HTTP 200", () => {
-      expect(response.statusCode).toBe(200);
-    });
-
-    it("should return products that match the parameter search", () => {
-      expect(response.body).toEqual(arrayOfProducts);
+    describe("given a query that doesnt match something in the database", () => {
+      it("should ", async () => {
+        for (const url of urls.bad) {
+          response = await request(app).get(url);
+          expect(response.statusCode).toBe(200);
+          expect(response.body).toEqual(emptyArray);
+        }
+      });
     });
   });
 
   describe("GET /products/:id", () => {
-    beforeAll(async () => {
-      response = await request(app).get(`/products/${productId}`);
+    describe("given a valid id", () => {
+      beforeAll(async () => {
+        response = await request(app).get(`/products/${productId}`);
+      });
+
+      it("should return HTTP 200", () => {
+        expect(response.statusCode).toBe(200);
+      });
+
+      it("should return a single product object", () => {
+        expect(response.body).toEqual(productObject);
+      });
     });
 
-    it("should return HTTP 200", () => {
-      expect(response.statusCode).toBe(200);
-    });
+    describe("given an invalid id", () => {
+      beforeAll(async () => {
+        response = await request(app).get(`/products/1999999999`);
+      });
 
-    it("should return a single product object", () => {
-      expect(response.body).toEqual(productObject);
+      it("should return HTTP 200", () => {
+        expect(response.statusCode).toBe(200);
+      });
+
+      it("should return an empty object", () => {
+        expect(response.body).toEqual(emptyObject);
+      });
     });
   });
 
   describe("PUT /products/:id", () => {
-    beforeAll(async () => {
-      response = await request(app).put(`/products/${productId}`).send({
-        name: "Great product name",
-        price: 350.0,
-        description: "What a great description",
-        category: "Fishing",
+    describe("given a bad id", () => {
+      beforeAll(async () => {
+        response = await request(app).put(`/products/1999999999`).send({
+          name: "Great product name",
+          price: 350.0,
+          description: "What a great description",
+          category: "Fishing",
+        });
+      });
+
+      it("should return HTTP 400", () => {
+        expect(response.statusCode).toBe(400);
+      });
+
+      it("should return an object with message property", () => {
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            message: expect.any(String),
+          })
+        );
       });
     });
-    it("should return HTTP 200", () => {
-      expect(response.statusCode).toBe(200);
-    });
 
-    it("should return the updated products object", () => {
-      expect(response.body).toEqual(productObject);
+    describe("given a valid id", () => {
+      beforeAll(async () => {
+        response = await request(app).put(`/products/${productId}`).send({
+          name: "Great product name",
+          price: 350.0,
+          description: "What a great description",
+          category: "Fishing",
+        });
+      });
+
+      it("should return HTTP 200", () => {
+        expect(response.statusCode).toBe(200);
+      });
+
+      it("should return the updated products object", () => {
+        expect(response.body).toEqual(productObject);
+      });
     });
   });
 
   describe("DELETE /products/:id", () => {
-    beforeAll(async () => {
-      response = await request(app).delete(`/products/${productId}`);
+    describe("given a bad id", () => {
+      beforeAll(async () => {
+        response = await request(app).delete(`/products/1999999999`);
+      });
+
+      it("should return HTTP 400", () => {
+        expect(response.statusCode).toBe(400);
+      });
+
+      it("should return an object with message property", () => {
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            message: expect.any(String),
+          })
+        );
+      });
     });
 
-    it("should return HTTP 204", () => {
-      expect(response.statusCode).toBe(204);
-    });
+    describe("given a valid id", () => {
+      beforeAll(async () => {
+        response = await request(app).delete(`/products/${productId}`);
+      });
 
-    it("should return nothing in the body", () => {
-      expect(response.body).toEqual(emptyObject);
+      it("should return HTTP 204", () => {
+        expect(response.statusCode).toBe(204);
+      });
+
+      it("should return nothing in the body", () => {
+        expect(response.body).toEqual(emptyObject);
+      });
     });
   });
 });
