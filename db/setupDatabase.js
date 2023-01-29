cf2d23478282cb013bec15125648892fe47178b4
mdwiltfong <wiltfong.michael1@gmail.com>
2023-01-29 17:32:35 -0500

    script smoothly connects to ecommerce_project

--- db/setupDatabase.js
+++ db/setupDatabase.js
@@ -29,7 +29,7 @@ const { DB } = require("../config");
       date             date,
       product_id       integer,
       FOREIGN KEY (product_id)
-        REFERENCES product(id) 
+        REFERENCES products(id) 
     );
   `;
 
@@ -55,38 +55,55 @@ const { DB } = require("../config");
 
   try {
     // Make a temporary client so we can create tables in the database
-    const db = new Client({
+    const dbPostGres = new Client({
       user: DB.PGUSER,
       host: DB.PGHOST,
       database: "postgres",
       password: DB.PGPASSWORD,
       port: DB.PGPORT,
     });
+    const dbECommerceProjectTest = new Client({
+      user: DB.PGUSER,
+      host: DB.PGHOST,
+      database: DB.PGDATABASE,
+      password: DB.PGPASSWORD,
+      port: DB.PGPORT,
+    });
+    /*
+    @hazeltonbw, although this secript is able to create the schema of the tables, it does not create the database itself.
+    Although you are not doing anything special with the database itself, I think the script is close enough to making it 
+    for contributors.
+    */
+    /*
+   @hazeltonbw First we'll connect to postgres in order to create the database. Typically postgres is the default db in PSQL. 
+   There is a chance that maybe a contributor doesn't have postgres locally. In which case it's up to the user to execute the SQL
+   themselves. You may be thinking this exception might defeat the whole purpose of trying to create the DB locally.
 
-    await db.connect();
+   I agree, and I think I found some stackoverflow articles that offer a good solution, although it will require restructuring the code.I'll
+   let you decide! 
+
+   https://stackoverflow.com/questions/20813154/node-postgres-create-database
+
+   */
+    await dbPostGres.connect();
     try {
-      await db.query(createDatabase);
+      await dbPostGres.query(createDatabase);
     } catch (error) {
       console.error(error);
-      console.debug("Switching to ecommerce_project");
-      db.database = DB.PGDATABASE;
-      await db.connect({
-        user: DB.PGUSER,
-        host: DB.PGHOST,
-        database: DB.PGDATABASE,
-        password: DB.PGPASSWORD,
-        port: DB.PGPORT,
-      });
+    } finally {
+      await dbPostGres.end();
+      console.debug(`Switching to ${DB.PGDATABASE}`);
+      await dbECommerceProjectTest.connect();
     }
 
     // Create tables on database
-    await db.query(createUsersTable);
-    await db.query(createProductsTable);
-    await db.query(createOrdersTable);
-    await db.query(createCartsTable);
-    await db.query(createCartItemsTable);
+    await dbECommerceProjectTest.query(createUsersTable);
+    await dbECommerceProjectTest.query(createProductsTable);
+    await dbECommerceProjectTest.query(createOrdersTable);
+    await dbECommerceProjectTest.query(createCartsTable);
+    await dbECommerceProjectTest.query(createCartItemsTable);
 
-    await db.end();
+    await dbECommerceProjectTest.end();
   } catch (err) {
     console.log("ERROR CREATING ONE OR MORE TABLES: ", err);
   }
