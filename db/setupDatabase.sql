ca6c0da7a20fd321a9804deed948ecab9985edfa
Brandon Hazelton <hazeltonbw@gmail.com>
2023-02-16 23:55:28 -0800

    update setupDatabase sql file to match new schema

--- db/setupDatabase.sql
+++ db/setupDatabase.sql
@@ -3,30 +3,6 @@
 BEGIN;
 
 
-DROP TABLE IF EXISTS public.cart;
-
-CREATE TABLE IF NOT EXISTS public.cart
-(
-    cart_id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
-    created date NOT NULL,
-    modified date NOT NULL,
-    PRIMARY KEY (cart_id)
-);
-
-DROP TABLE IF EXISTS public.product;
-
-CREATE TABLE IF NOT EXISTS public.product
-(
-    product_id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
-    category_id integer NOT NULL,
-    title text NOT NULL,
-    price numeric(5, 2) NOT NULL,
-    description text,
-    PRIMARY KEY (product_id)
-);
-
-DROP TABLE IF EXISTS public.cart_has_products;
-
 CREATE TABLE IF NOT EXISTS public.cart_has_products
 (
     cart_id integer NOT NULL,
@@ -34,111 +10,117 @@ CREATE TABLE IF NOT EXISTS public.cart_has_products
     qty integer NOT NULL
 );
 
-DROP TABLE IF EXISTS public."order";
-
-CREATE TABLE IF NOT EXISTS public."order"
+CREATE TABLE IF NOT EXISTS public.carts
 (
-    order_id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
-    user_id integer NOT NULL,
-    date date NOT NULL,
-    status text NOT NULL,
-    PRIMARY KEY (order_id)
+    cart_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    created date NOT NULL,
+    modified date NOT NULL,
+    CONSTRAINT carts_pkey PRIMARY KEY (cart_id)
 );
 
-DROP TABLE IF EXISTS public."user";
-
-CREATE TABLE IF NOT EXISTS public."user"
+CREATE TABLE IF NOT EXISTS public.categories
 (
-    user_id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
-    cart_id integer,
-    fname character varying(20),
-    lname character varying(20),
-    email character varying(70),
-    PRIMARY KEY (user_id)
+    category_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    name text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT categories_pkey PRIMARY KEY (category_id)
 );
 
-DROP TABLE IF EXISTS public.order_has_product;
-
-CREATE TABLE IF NOT EXISTS public.order_has_product
+CREATE TABLE IF NOT EXISTS public.order_has_products
 (
     order_id integer NOT NULL,
     product_id integer NOT NULL,
     qty integer NOT NULL
 );
 
-DROP TABLE IF EXISTS public.category;
-
-CREATE TABLE IF NOT EXISTS public.category
+CREATE TABLE IF NOT EXISTS public.orders
 (
-    category_id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
-    name text NOT NULL,
-    PRIMARY KEY (category_id)
+    order_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    user_id integer NOT NULL,
+    date date NOT NULL,
+    status text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT orders_pkey PRIMARY KEY (order_id)
 );
 
-DROP TABLE IF EXISTS public.user_sessions;
+CREATE TABLE IF NOT EXISTS public.products
+(
+    product_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    category_id integer NOT NULL,
+    title text COLLATE pg_catalog."default" NOT NULL,
+    price numeric(5, 2) NOT NULL,
+    description text COLLATE pg_catalog."default",
+    CONSTRAINT products_pkey PRIMARY KEY (product_id)
+);
 
-CREATE TABLE IF NOT EXISTS "user_sessions" (
-  "sid" varchar NOT NULL COLLATE "default",
-  "sess" json NOT NULL,
-  "expire" timestamp(6) NOT NULL
-)
-WITH (OIDS=FALSE);
+CREATE TABLE IF NOT EXISTS public.user_sessions
+(
+    sid character varying COLLATE pg_catalog."default" NOT NULL,
+    sess json NOT NULL,
+    expire timestamp(6) without time zone NOT NULL,
+    CONSTRAINT session_pkey PRIMARY KEY (sid)
+);
 
-ALTER TABLE IF EXISTS "user_sessions" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+CREATE TABLE IF NOT EXISTS public.users
+(
+    user_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    cart_id integer,
+    fname character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    lname character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(70) COLLATE pg_catalog."default" NOT NULL,
+    password text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT users_pkey PRIMARY KEY (user_id)
+);
 
-CREATE INDEX "IDX_session_expire" ON "user_sessions" ("expire");
-
-ALTER TABLE IF EXISTS public.product
-    ADD FOREIGN KEY (category_id)
-    REFERENCES public.category (category_id) MATCH SIMPLE
+ALTER TABLE IF EXISTS public.cart_has_products
+    ADD CONSTRAINT cart_has_products_cart_id_fkey FOREIGN KEY (cart_id)
+    REFERENCES public.carts (cart_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE CASCADE
     NOT VALID;
 
 
 ALTER TABLE IF EXISTS public.cart_has_products
-    ADD FOREIGN KEY (cart_id)
-    REFERENCES public.cart (cart_id) MATCH SIMPLE
+    ADD CONSTRAINT cart_has_products_product_id_fkey FOREIGN KEY (product_id)
+    REFERENCES public.products (product_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE CASCADE
     NOT VALID;
 
 
-ALTER TABLE IF EXISTS public.cart_has_products
-    ADD FOREIGN KEY (product_id)
-    REFERENCES public.product (product_id) MATCH SIMPLE
+ALTER TABLE IF EXISTS public.order_has_products
+    ADD CONSTRAINT order_has_products_order_id_fkey FOREIGN KEY (order_id)
+    REFERENCES public.orders (order_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE CASCADE
     NOT VALID;
 
 
-ALTER TABLE IF EXISTS public."order"
-    ADD FOREIGN KEY (user_id)
-    REFERENCES public."user" (user_id) MATCH SIMPLE
+ALTER TABLE IF EXISTS public.order_has_products
+    ADD CONSTRAINT order_has_products_product_id_fkey FOREIGN KEY (product_id)
+    REFERENCES public.products (product_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE CASCADE
     NOT VALID;
 
 
-ALTER TABLE IF EXISTS public."user"
-    ADD FOREIGN KEY (cart_id)
-    REFERENCES public.cart (cart_id) MATCH SIMPLE
+ALTER TABLE IF EXISTS public.orders
+    ADD CONSTRAINT orders_user_id_fkey FOREIGN KEY (user_id)
+    REFERENCES public.users (user_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE CASCADE
     NOT VALID;
 
 
-ALTER TABLE IF EXISTS public.order_has_product
-    ADD FOREIGN KEY (order_id)
-    REFERENCES public."order" (order_id) MATCH SIMPLE
+ALTER TABLE IF EXISTS public.products
+    ADD CONSTRAINT products_category_id_fkey FOREIGN KEY (category_id)
+    REFERENCES public.categories (category_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE CASCADE
     NOT VALID;
 
 
-ALTER TABLE IF EXISTS public.order_has_product
-    ADD FOREIGN KEY (product_id)
-    REFERENCES public.product (product_id) MATCH SIMPLE
+ALTER TABLE IF EXISTS public.users
+    ADD CONSTRAINT users_cart_id_fkey FOREIGN KEY (cart_id)
+    REFERENCES public.carts (cart_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE CASCADE
     NOT VALID;
