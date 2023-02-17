f962efed55b9da0396484376149dbe3848b5d378
Brandon Hazelton <hazeltonbw@gmail.com>
2023-02-16 23:34:22 -0800

    changed seedDatabase file to match updated database schema

--- db/seedDatabase.js
+++ db/seedDatabase.js
@@ -1,59 +1,57 @@
-const { query } = require("express");
+//const { query } = require("express");
 const { Client } = require("pg");
 const { DB } = require("../config");
 const dotenv = require("dotenv").config({
   path: "../.env",
 });
 (async () => {
-  const clearUsersTable = `
-    DELETE FROM users
-    `;
-  const clearProductsTable = `
-    DELETE FROM products
-    `;
-  const clearOrdersTable = `
-    DELETE FROM orders`;
-  const clearCartsTable = `
-    DELETE FROM carts
-    `;
-  const clearCartItemsTable = `
-    DELETE FROM cartitems
-    `;
+  const clearUsersTable = `DELETE FROM users`;
+  const clearProductsTable = `DELETE FROM products`;
+  const clearOrdersTable = `DELETE FROM orders`;
+  const clearCartsTable = `DELETE FROM carts`;
+  const clearCartItemsTable = `DELETE FROM cart_has_products`;
   // Users whose passwords are "password"
   const seedUsersTable = `
-    INSERT INTO users OVERRIDING SYSTEM VALUE
+    INSERT INTO users(user_id, fname, lname, email, password) OVERRIDING SYSTEM VALUE
     VALUES
-    (1,'password','f1.l1@test.com'),
-    (2,'password','f2.l2@test.com'),
-    (3,'password','f3.l3@test.com')
+    (1,'f1', 'l1', 'f1.l1@test.com', 'password'),
+    (2,'f2', 'l2', 'f2.l2@test.com', 'password'),
+    (3,'f3', 'l3', 'f3.l3@test.com', 'password')
+  `;
+
+  const seedCategoriesTable = `
+    INSERT INTO categories(name) OVERRIDING SYSTEM VALUE
+    VALUES 
+    ('Clothes');
   `;
 
   const seedProductsTable = `
-    INSERT INTO products OVERRIDING SYSTEM VALUE
+    INSERT INTO products(product_id, category_id, title, price, description) OVERRIDING SYSTEM VALUE
      VALUES
-    (1,'pants',123.45,'Description 1','Test1'),
-    (2,'shirt',10.00,'Description 2','Test2'),
-    (3,'pants',5.00,'Description 3','Test1'),
-    (4,'pants',9999.00,'Description 1','Test2');
+    (1, 1,'blue pants',  123.45,  'Description 1'),
+    (2, 1,'shirt',       10.00,   'Description 2'),
+    (3, 1,'black pants', 5.00,    'Description 3'),
+    (4, 1,'khaki pants', 999.00, 'Description 4');
   `;
 
   const seedOrdersTable = `
-    INSERT INTO orders
+    INSERT INTO orders(user_id, date, status)
     OVERRIDING SYSTEM VALUE
     VALUES
-   (1,'1992-09-25',2),
-   (2,'2023-01-29',3)
+   (1,'1992-09-25','COMPLETE'),
+   (2,'2023-01-29','PENDING')
   `;
 
   const seedCartsTable = `
-    INSERT INTO carts
+    INSERT INTO carts(cart_id, created, modified)
     OVERRIDING SYSTEM VALUE
      VALUES
     (1,'2023-09-25','2023-09-26')
   `;
 
   const seedCartItemsTable = `
-    INSERT INTO cartitems OVERRIDING SYSTEM VALUE
+    INSERT INTO cart_has_products 
+    OVERRIDING SYSTEM VALUE
      VALUES
     (1,1,1)
   `;
@@ -80,6 +78,7 @@ const dotenv = require("dotenv").config({
 
     // Seed tables on database
     await dbECommerceProjectTest.query(seedUsersTable);
+    await dbECommerceProjectTest.query(seedCategoriesTable);
     await dbECommerceProjectTest.query(seedProductsTable);
     await dbECommerceProjectTest.query(seedOrdersTable);
     await dbECommerceProjectTest.query(seedCartsTable);
