16721aceb5d6be4ec56d27e7d06a78770c9789fe
Brandon Hazelton <hazeltonbw@gmail.com>
2023-03-02 03:18:56 -0800

    check for items in cart before heading to checkout

---
+++ middleware/cart.js
@@ -0,0 +1,19 @@
+const cartModel = require("../models/cart");
+
+const hasItemsInCart = async (req, res, next) => {
+  const { user_id } = req.user;
+  try {
+    const cart = await cartModel.getCartByUserId(user_id);
+    if (!cart) {
+      console.log("nothing in ur cart bro");
+      return res.status(400).send({ message: "No items in your cart!" });
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  hasItemsInCart,
+};
